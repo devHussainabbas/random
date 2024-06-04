07966fd47d228098aea192e47b408d554af834df
devHussainabbas <03555abc@gmail.com>
2024-06-04 21:39:40 +0500

    loops

---
+++ daily class/app.js
@@ -0,0 +1,118 @@
+// let subjects=["Math","Computer","Islamiat","History","Psychology","Statistics"];
+// console.log(subjects)
+
+
+// subjects.push("husssain")
+
+// let z=['f','e','c','d','a','b']
+// console.log(z);
+// let c=z.sort()
+// console.log(c);
+
+// let d=[5,3,1]
+// console.log(d)
+// let e=d.sort()
+// console.log(e)
+
+
+// let y=subjects.slice(4)
+// console.log(y);
+
+// subjects.splice(2,2,"a","b")
+// console.log(subjects)
+
+
+
+// document.write(subjects)
+// let a=subjects[0];
+// console.log("my first subject is" +" "+ subjects[0],"\nmy second subject is" +" "+ subjects[1],"\nmy third subject is" +" "+ subjects[2])
+// console.log(`my first subject is ${subjects[0]},\nmy second subject is ${subjects[1]},\nmy third subject is ${subjects[2]},\nmy fourth subject is ${subjects[3]},\nmy fifth subject is ${subjects[4]},\nmy sixth subject is ${subjects[5]}`);
+
+// let b=subjects[1];
+// console.log("my  subject is" +" "+ b)
+
+
+
+
+
+
+// subjects.push("Urdu","Geography","Civics");
+// document.write(subjects)
+// console.log(subjects)
+// console.log(subjects.length)
+
+
+
+// subjects.unshift("education","English","Pak Study");
+// document.write(subjects);
+// console.log(subjects)
+// console.log(subjects.length)
+
+//  alert("hussain")
+
+//  prompt("can you apply this website page")
+//  confirm("ok")
+
+
+// let num = 8
+// let num2 = 5
+
+// let results = num !== num2
+
+// document.write(results)
+
+
+// let x = 5;
+
+// let result = x <= 6 ;
+// document.write(result) 
+
+// let nmbr=prompt("enter a number for check")
+// if (nmbr=="jamshad"){
+// console.log("jamshad ius university student");
+// } else if(nmbr=="hussain"){
+//     console.log("hussaian ius college student");
+// } else if(nmbr=="rayyan"){
+//     console.log("software developer");
+// }
+// else{
+//     console.log("not a student");
+// }
+
+
+// let data=prompt("enter")
+// switch(true){
+//     case(data>18):console.log("Eligible");
+//     break;
+//     case(data<18):console.log("not Eligible");
+//     break;
+//     default:console.log("error");
+// }
+// let a=29;
+
+// let b=20
+// let a;
+// a=(b>18)?"true":"false";
+// console.log(a);
+
+// let result=(2<18)?"true":"false";console.log(result);
+
+// let data=0;
+// while(data<99){
+// data=data+1;
+// console.log(data);
+// }
+
+// let o=0
+// while(o<9){
+//     o++;
+//     console.log(o);
+// }
+
+let text="";
+let o = 1;
+while (o <= 20) {
+  text=text+"\n"+o;
+  o=o+2;
+}
+console.log(text);
